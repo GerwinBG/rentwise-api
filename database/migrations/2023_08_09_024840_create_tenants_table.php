b2aa8ee1c5ea8a9d8f1273f5841894348a12e00a
Gerwin Gindoy <gerwingindoy@gmail.com>
2023-08-09 11:50:19 +0800

    first commit

---
+++ database/migrations/2023_08_09_024840_create_tenants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('apartment_id');
+            $table->string('email');
+            $table->string('contactNumber');
+            $table->unsignedInteger('occupantsQty');
+            $table->timestamps();
+
+            $table->foreign('apartment_id')->references('id')->on('apartments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tenants');
+    }
+};
